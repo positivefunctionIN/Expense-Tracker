4ea1e68954165d934df9c349a0f745ce1d0a93eb
Ananaya <ananaya20may@gmail.com>
2026-04-15 23:57:30 +0530

    entity

--- app/src/main/java/com/example/expensetracker/ui/database/Expense.kt
+++ app/src/main/java/com/example/expensetracker/ui/database/Expense.kt
@@ -1,8 +1,12 @@
 package com.example.expensetracker.ui.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "expenses")
 data class Expense (
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val amount: Double,
     val description: String,
     val category: String,
