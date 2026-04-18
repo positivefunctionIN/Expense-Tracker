2c7d3fc586e3e35e446e2d4ff431998d7d3b4631
Ananaya <ananaya20may@gmail.com>
2026-04-18 21:19:14 +0530

    INSERT added

--- app/src/main/java/com/example/expensetracker/data/local/expenseDAO.kt
+++ app/src/main/java/com/example/expensetracker/data/local/expenseDAO.kt
@@ -1,5 +1,19 @@
 package com.example.expensetracker.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import androidx.room.OnConflictStrategy
 
 @Dao
+interface ExpenseDao {
+    @Query("SELECT * FROM expenses ORDER BY date DESC")
+    suspend fun getAllExpenses(): Flow<List<ExpenseEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExpense(expense: ExpenseEntity)
+
+
+}
