dcde504f687a8fd827cc1cc4ec9c6a99f6d90570
Ananaya <ananaya20may@gmail.com>
2026-05-17 22:51:16 +0530

    Expense Tracker

--- app/src/main/java/com/example/expensetracker/data/root/expense tracker.kt
+++
@@ -1,8 +0,0 @@
-package com.example.expensetracker.data.root
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ExpenseTrackerApp : Application()
-
