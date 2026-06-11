4f53b4bdcdbb7a5905bdbfa82ac20d9d630cb6bb
Ananaya <ananaya20may@gmail.com>
2026-06-12 00:35:42 +0530

    Colors theme

--- app/src/main/java/com/example/expensetracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/expensetracker/ui/theme/Color.kt
@@ -1,11 +1,32 @@
+// ui/theme/Color.kt
 package com.example.expensetracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary colors
+val Primary = Color(0xFF6200EE)
+val PrimaryVariant = Color(0xFF3700B3)
+val Secondary = Color(0xFF03DAC6)
+val SecondaryVariant = Color(0xFF018786)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Background colors
+val Background = Color(0xFFFAFAFA)
+val Surface = Color(0xFFFFFFFF)
+val Error = Color(0xFFB00020)
+
+// Text colors
+val OnPrimary = Color(0xFFFFFFFF)
+val OnSecondary = Color(0xFF000000)
+val OnBackground = Color(0xFF1F1F1F)
+val OnSurface = Color(0xFF1F1F1F)
+
+// Category colors (used in ExpenseItem)
+val FoodColor = Color(0xFFFF6B6B)
+val TransportColor = Color(0xFF4ECDC4)
+val EntertainmentColor = Color(0xFFFFE66D)
+val ShoppingColor = Color(0xFF95E1D3)
+val UtilitiesColor = Color(0xFFC7CEEA)
+val HealthcareColor = Color(0xFFFF6B9D)
+val EducationColor = Color(0xFF74B9FF)
+val TravelColor = Color(0xFFA29BFE)
+val OtherColor = Color(0xFFDFE6E9)
